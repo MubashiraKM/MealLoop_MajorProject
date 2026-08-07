58537cf669bd601a6cb7a7ea9d8e07ff5a5c21d2
MubashiraKM <167055008+MubashiraKM@users.noreply.github.com>
2026-08-07 12:29:34 +0530

    Update settings.py

--- smart_foodwaste_management_project/smart_foodwaste_management_project/settings.py
+++ smart_foodwaste_management_project/smart_foodwaste_management_project/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mealloop@gmail.com'
-EMAIL_HOST_PASSWORD = 'czqfccpfojmadioc'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'passwaord'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
